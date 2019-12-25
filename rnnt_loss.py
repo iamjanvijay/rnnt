4e8ebfe88e76f32d64c6c00fa39e594645154d73
Abhinav Goyal <abhinav.goyal@flipkart.com>
2019-12-25 17:01:00 +0530

    dp

--- rnnt_loss.py
+++ rnnt_loss.py
@@ -12,3 +12,28 @@ def transition_probs(labels, log_probs):
     truth_probs = tf.reduce_sum(tf.multiply(log_probs, labels), axis=-1)
 
     return blank_probs, truth_probs
+
+
+def next_state(alpha, trans_probs):
+    blank_probs = trans_probs[0]
+    truth_probs = trans_probs[1]
+
+    alpha_b = tf.concat([tf.math.log(tf.zeros(shape=[alpha.shape[0], 1])), alpha[:, :-1] + blank_probs], axis=1)
+    alpha_t = tf.concat([alpha[:, :-1] + truth_probs, tf.math.log(tf.zeros(shape=[alpha.shape[0], 1]))], axis=1)
+
+    alpha = tf.reduce_logsumexp(tf.stack([alpha_b, alpha_t], axis=0), axis=0)
+    return alpha
+
+
+def rnnt_loss_and_grad(logits, labels, label_length, logit_length):
+    log_probs = tf.nn.log_softmax(logits)
+
+    initial_alpha = tf.zeros(shape=[labels.shape[0], labels.shape[1]+1])
+    initial_alpha[:, 0] = 1.0
+    initial_alpha = tf.math.log(initial_alpha)
+
+    trans_probs = transition_probs(labels, log_probs)
+
+    fwd = tf.scan(next_state, trans_probs, initializer=initial_alpha)
+    alpha = tf.transpose(tf.concat([initial_alpha, fwd], axis=0), perm=[1, 2, 0])
+
