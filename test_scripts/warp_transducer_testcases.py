492f094c38ecf042120fae96fb51b53068662a0f
mejanvijay <janvijay.singh@dev26.node.consul.liv.ai>
2019-12-25 07:27:58 +0000

    debugging test_script

--- test_scripts/warp_transducer_testcases.py
+++ test_scripts/warp_transducer_testcases.py
@@ -8,20 +8,20 @@ def gen_test_case(batch_num, max_label_length, max_input_length, output_vocab_si
 	blank_label = 0 # assuming zero for now; np.random.randint(low=0, high=output_vocab_size+1, size=())
 	input_lengths = np.random.randint(low=1, high=max_input_length+1, size=(batch_num))
 	labels = np.random.randint(low=1, high=output_vocab_size+1, size=(batch_num, max_label_length))
-	acts = np.random.rand(batch_num, max_input_length, max_label_length, output_vocab_size+1)
+	acts = np.random.rand(batch_num, max_input_length, max_label_length+1, output_vocab_size+1).astype(np.float32)
 	log_probs = tf.nn.log_softmax(acts, axis=3)
 	
 	# Outputs
-	final_loss = warprnnt_tensorflow.rnnt_loss(acts, labels, input_lengths, label_lengths, blank_label)
-
-	return {'acts': acts, 'log_probs': log_probs, 'labels': labels, 'input_lengths': input_lengths, 'label_lengths': label_lengths, 'blank_label': blank_label, 'final_loss': final_loss}
+	final_loss = warprnnt_tensorflow.rnnt_loss(log_probs, labels, input_lengths, label_lengths, blank_label)
+	
+	return {'acts': acts, 'log_probs': log_probs.numpy, 'labels': labels, 'input_lengths': input_lengths, 'label_lengths': label_lengths, 'blank_label': blank_label, 'final_loss': final_loss.numpy}
 	
 if __name__ == '__main__':
 	batch_num = 1
 	max_label_length = 5
 	max_input_length = 5
 	output_vocab_size = 3 # Without blank symbol
-	num_test_cases = 5
+	num_test_cases = 10
 	
 	for i in range(num_test_cases):
 		test_case = gen_test_case(batch_num, max_label_length, max_input_length, output_vocab_size)
