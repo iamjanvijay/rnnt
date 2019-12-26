removed bug in batch loss computation

rnnt_loss.py
@@ -51,4 +51,10 @@ def rnnt_loss_and_grad(logits, labels, label_length, logit_length):
     fwd = tf.scan(next_state, trans_probs_diags, initializer=initial_alpha)
     alpha = tf.transpose(tf.concat([tf.expand_dims(initial_alpha, axis=0), fwd], axis=0), perm=[1, 2, 0])
 
-    return -alpha[:, -1, -1]
+    alpha_i = tf.stack([logit_length - 1, logit_length + label_length - 1], axis=1)
+    blank_i = tf.stack([logit_length - 1, label_length], axis=1)
+
+    final_state_probs = tf.gather_nd(alpha, alpha_i, batch_dims=1) + tf.gather_nd(blank_probs, blank_i, batch_dims=1)
+
+    loss = -final_state_probs
+    return loss

run_tests.py
@@ -15,8 +15,10 @@ if __name__ == '__main__':
         data = np.load(i, allow_pickle=True).item()
         logits = tf.convert_to_tensor(data['acts'])
         labels = tf.convert_to_tensor(data['labels'])
+        label_lengths = tf.convert_to_tensor(data['label_lengths'])
+        logit_lengths = tf.convert_to_tensor(data['input_lengths'])
         true_loss = np.array(data['final_loss'])
-        pred_loss = np.array(rnnt_loss_and_grad(logits, labels, None, None))
+        pred_loss = np.array(rnnt_loss_and_grad(logits, labels, label_lengths, logit_lengths))
         passed[i] = (np.sum(np.abs(true_loss - pred_loss)) < EPS)
 
     print(passed)

test_scripts/warp_transducer_testcases.py
@@ -11,13 +11,17 @@ def gen_test_case(batch_num, max_label_length, max_input_length, output_vocab_si
 	input_lengths = np.random.randint(low=1, high=max_input_length+1, size=batch_num)
 	# input_lengths = np.asarray([max_input_length] * batch_num)
 	labels = np.random.randint(low=1, high=output_vocab_size+1, size=(batch_num, max_label_length))
-	acts = np.random.rand(batch_num, max_input_length, max_label_length+1, output_vocab_size+1).astype(np.float32)
-	log_probs = tf.nn.log_softmax(acts, axis=3)
+	with tf.GradientTape() as g:
+		acts = tf.convert_to_tensor(np.random.rand(batch_num, max_input_length, max_label_length+1, output_vocab_size+1).astype(np.float32))
+		g.watch(acts)
+		log_probs = tf.nn.log_softmax(acts, axis=3)
+
+		# Outputs
+		final_loss = warprnnt_tensorflow.rnnt_loss(log_probs, labels, input_lengths, label_lengths, blank_label)
+
+	grads = g.gradient(final_loss, acts)
 	
-	# Outputs
-	final_loss = warprnnt_tensorflow.rnnt_loss(log_probs, labels, input_lengths, label_lengths, blank_label)
-	
-	return {'acts': acts, 'log_probs': log_probs.numpy, 'labels': labels, 'input_lengths': input_lengths, 'label_lengths': label_lengths, 'blank_label': blank_label, 'final_loss': final_loss.numpy()}
+	return {'acts': acts, 'log_probs': log_probs.numpy, 'labels': labels, 'input_lengths': input_lengths, 'label_lengths': label_lengths, 'blank_label': blank_label, 'final_loss': final_loss.numpy(), 'grads': grads.numpy()}
 
 
 if __name__ == '__main__':